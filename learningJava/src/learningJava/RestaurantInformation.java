029e12946dd7cd4911bfe8944ee956de6eae13bd
Emily Mullin <emily.mullin@csuglobal.edu>
2025-06-02 01:28:56 -0600

    First commit and added RestaurantInformation Class

---
+++ learningJava/src/learningJava/RestaurantInformation.java
@@ -0,0 +1,15 @@
+package learningJava;
+
+public class RestaurantInformation {
+
+	public static void main(String[] args) {
+		 
+		System.out.println ("Texas Roadhouse");
+		 
+		 System.out.println ("3120 North Powers Boulevard");
+		 
+		 System.out.println ("Colorado Springs, CO, 80922");
+
+	}
+
+}
